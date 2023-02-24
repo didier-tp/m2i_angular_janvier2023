00ca51eb0edb71d39676a3f757347655c5d922f8
DidierDefrance <didier@d-defrance.fr>
2023-02-24 12:18:51 +0100

    s

--- typescript_24fevrier_2023/tp/dist/js/personne.js
+++ typescript_24fevrier_2023/tp/dist/js/personne.js
@@ -1,9 +1,21 @@
 "use strict";
+/*
+//V1 sans constructor:
 class Personne {
-    constructor() {
-        this.prenom = "";
-        this.nom = "";
-        this.age = 0;
+    prenom : string = "";
+    nom : string ="";
+    age : number =0 ;
+
+    incrementerAge(){
+        this.age=this.age+1;
+    }
+}
+*/
+class Personne {
+    constructor(prenom = "", nom = "", age = 0) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.age = age;
     }
     incrementerAge() {
         this.age = this.age + 1;
@@ -17,3 +29,5 @@ p1.age = 40;
 p1.incrementerAge();
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
 console.log(JSON.stringify(p1));
+let p2 = new Personne("alain", "Therieur", 30);
+console.log(JSON.stringify(p2));
--- typescript_24fevrier_2023/tp/src/personne.ts
+++ typescript_24fevrier_2023/tp/src/personne.ts
@@ -1,7 +1,28 @@
+/*
+//V1 sans constructor:
 class Personne {
     prenom : string = "";
     nom : string ="";
-    age : number = 0;
+    age : number =0 ;
+
+    incrementerAge(){
+        this.age=this.age+1;
+    }
+}
+*/
+
+class Personne {
+    prenom : string ;
+    nom : string ;
+    age : number ;
+
+    constructor(prenom : string = "", 
+                nom : string = "",
+                age : number = 0 ){
+       this.prenom = prenom;
+       this.nom = nom;
+       this.age = age;
+    }
 
     incrementerAge(){
         this.age=this.age+1;
@@ -14,3 +35,6 @@ p1.prenom = "jean"; p1.nom = "Bon"; p1.age=40;
 p1.incrementerAge();
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
 console.log(JSON.stringify(p1));
+
+let p2 = new Personne("alain" , "Therieur" , 30);
+console.log(JSON.stringify(p2));
