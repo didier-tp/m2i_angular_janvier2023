ebf5571daa57d342c814b5d5e750199d2ea2ce48
DidierDefrance <didier@d-defrance.fr>
2023-02-24 11:47:46 +0100

    s

--- typescript_24fevrier_2023/tp/dist/js/personne.js
+++ typescript_24fevrier_2023/tp/dist/js/personne.js
@@ -8,4 +8,8 @@ class Personne {
 }
 let p1;
 p1 = new Personne();
+p1.prenom = "jean";
+p1.nom = "Bon";
+p1.age = 40;
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
+console.log(JSON.stringify(p1));
--- typescript_24fevrier_2023/tp/src/personne.ts
+++ typescript_24fevrier_2023/tp/src/personne.ts
@@ -6,4 +6,6 @@ class Personne {
 
 let p1 : Personne ;
 p1 = new Personne();
+p1.prenom = "jean"; p1.nom = "Bon"; p1.age=40;
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
+console.log(JSON.stringify(p1));
