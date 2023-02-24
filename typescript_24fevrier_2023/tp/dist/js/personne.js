bbd610ebf7b6cd28d28482e8c6721eebf94ef3b9
DidierDefrance <didier@d-defrance.fr>
2023-02-24 11:51:19 +0100

    s

--- typescript_24fevrier_2023/tp/dist/js/personne.js
+++ typescript_24fevrier_2023/tp/dist/js/personne.js
@@ -5,11 +5,15 @@ class Personne {
         this.nom = "";
         this.age = 0;
     }
+    incrementerAge() {
+        this.age = this.age + 1;
+    }
 }
 let p1;
 p1 = new Personne();
 p1.prenom = "jean";
 p1.nom = "Bon";
 p1.age = 40;
+p1.incrementerAge();
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
 console.log(JSON.stringify(p1));
--- typescript_24fevrier_2023/tp/src/personne.ts
+++ typescript_24fevrier_2023/tp/src/personne.ts
@@ -2,10 +2,15 @@ class Personne {
     prenom : string = "";
     nom : string ="";
     age : number = 0;
+
+    incrementerAge(){
+        this.age=this.age+1;
+    }
 }
 
 let p1 : Personne ;
 p1 = new Personne();
 p1.prenom = "jean"; p1.nom = "Bon"; p1.age=40;
+p1.incrementerAge();
 console.log(p1.prenom + " " + p1.nom + " " + p1.age);
 console.log(JSON.stringify(p1));
