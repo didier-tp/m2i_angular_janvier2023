2ed1addcabe2fbcb1b505d57f07a3dc4f38bb372
DidierDefrance <didier@d-defrance.fr>
2023-02-27 10:13:58 +0100

    components

---
+++ angular_fevrier_2023/my-app/src/app/basic/tva/tva.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tva',
+  templateUrl: './tva.component.html',
+  styleUrls: ['./tva.component.css']
+})
+export class TvaComponent {
+
+}
