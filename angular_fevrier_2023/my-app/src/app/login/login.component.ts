3c468475f1f6e213e5ecd044d313e557e1463e06
DidierDefrance <didier@d-defrance.fr>
2023-02-28 11:38:29 +0100

    debut login

--- angular_fevrier_2023/my-app/src/app/login/login.component.ts
+++ angular_fevrier_2023/my-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Login } from '../common/data/login';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public login : Login = new Login();
 
+  public message :string ="";
+  
+  public onLogin(){
+     this.message = "donnees saisies = " + JSON.stringify(this.login);
+  }
 }
