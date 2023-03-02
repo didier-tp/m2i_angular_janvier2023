767cf83a9959390f139f7996c4806727e0620e2f
DidierDefrance <didier@d-defrance.fr>
2023-03-02 09:28:42 +0100

    s

--- angular_fevrier_2023/my-app/src/app/login/login.component.ts
+++ angular_fevrier_2023/my-app/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { Login } from '../common/data/login';
 import { LoginService } from '../common/service/login.service';
 import { firstValueFrom } from 'rxjs';
 import { messageFromError } from '../common/util/utils';
+import { SessionService } from '../common/service/session.service';
+
 
 @Component({
   selector: 'app-login',
@@ -12,18 +14,23 @@ import { messageFromError } from '../common/util/utils';
 export class LoginComponent {
   public login : Login = new Login("admin1","pwdadmin1","admin");
 
-  constructor(private _loginService : LoginService){}
+  constructor(private _loginService : LoginService ,
+              public sessionService : SessionService){}
 
   public message :string ="";
-  public isConnected = false;
+  
 
   public async onLogin(){
      console.log("donnees saisies = " + JSON.stringify(this.login));
      try{
-       this.isConnected = false;
+       this.sessionService.isConnected = false;
        let loginResponse  = await firstValueFrom( this._loginService.postLogin$(this.login) );
        this.message = loginResponse.message;
-       this.isConnected = loginResponse.status;
+       this.sessionService.isConnected = loginResponse.status;
+       if(loginResponse.status)
+               this.sessionService.username = loginResponse.username;
+           else
+              this.sessionService.username = "?"
      }catch(ex){
       this.message = messageFromError(<any> ex , "echec login ");
       console.log("error:"+ this.message );
