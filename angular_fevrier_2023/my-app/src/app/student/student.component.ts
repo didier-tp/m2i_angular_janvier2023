d19ab321c0ec8f5397a6f75635507a9ffc07a3e8
DidierDefrance <didier@d-defrance.fr>
2023-03-02 10:36:21 +0100

    s

--- angular_fevrier_2023/my-app/src/app/student/student.component.ts
+++ angular_fevrier_2023/my-app/src/app/student/student.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Student } from '../common/data/student';
+import { StudentService } from '../common/service/student.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-student',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent {
+
+  studentList : Student[] = []; //à afficher
+
+  constructor(public studentService : StudentService){
+    this.recupererStudents();
+  }
+
+  async recupererStudents(){
+    try{
+       this.studentList = await firstValueFrom(this.studentService.getAllStudents$());
+    }catch(err){
+       console.log("err="+err);
+    }
+  }
 
 }
