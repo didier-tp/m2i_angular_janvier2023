a254178d22d5abdfd52f0727754db512cfb6232b
DidierDefrance <didier@d-defrance.fr>
2023-03-02 12:05:00 +0100

    s

--- angular_fevrier_2023/my-app/src/app/common/service/student.service.ts
+++ angular_fevrier_2023/my-app/src/app/common/service/student.service.ts
@@ -8,6 +8,9 @@ import { Student } from '../data/student';
 })
 export class StudentService {
 
+  //private _apiBaseUrl ="https://localhost:7250/api/Students";  //possible si autorisations CORS dans le serveur .net
+  // si debut d'url incomplete en "/api/Students" , le navigateur
+  // va déclencher une url complete http://localhost:4200/api/Students et donc on passe par le serveur intermédiaire ng serve
   private _apiBaseUrl ="/api/Students";//appel indirect via reverse-proxy (de ng-serve ou autre) ne nécessitant pas autorisations CORS
   //necessite ng serve --proxy-config proxy.conf.json ou équivalent
 
