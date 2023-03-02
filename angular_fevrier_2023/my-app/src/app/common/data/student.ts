ea6321c8900f9d97768a782852c1f2bf9985df61
DidierDefrance <didier@d-defrance.fr>
2023-03-02 09:54:51 +0100

    s

---
+++ angular_fevrier_2023/my-app/src/app/common/data/student.ts
@@ -0,0 +1,8 @@
+export class Student {
+    constructor(
+        public ID :number = 0,
+        public FirstMidName : string = "",
+        public LastName : string = "",
+        public EnrollmentDate : string ="2000-01-01"
+    ){}
+}
