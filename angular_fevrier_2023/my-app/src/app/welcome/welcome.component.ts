20078c234dc05f85535829a5a7479047d66243c2
DidierDefrance <didier@d-defrance.fr>
2023-03-03 13:38:04 +0100

    enum exemple dans welcome

--- angular_fevrier_2023/my-app/src/app/welcome/welcome.component.ts
+++ angular_fevrier_2023/my-app/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Genre } from '../common/data/genre';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  Genre = Genre;
+  genre : Genre = Genre.Inconnu;
+
+  keysOfGenre() : Array<any> {
+    var keys = Object.keys(this.Genre);
+    return keys.slice(keys.length / 2);
+}
+
+  enumGenreAsText(g : Genre){
+    return Genre[g];
+  }
 
 }
